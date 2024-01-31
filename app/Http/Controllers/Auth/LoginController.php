dc1cd9b2ad3a7e8205b4da6f53b80fd1a4688511
Rong9394 <148251361+Rong9394@users.noreply.github.com>
2024-01-31 17:56:08 -0500

    changed login redirection to /posts

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -20,6 +21,11 @@ class LoginController extends Controller
     */
 
     use AuthenticatesUsers;
+
+    protected function authenticated(Request $request, $user)
+    {
+        return redirect()->route('posts');
+    }
 
     /**
      * Where to redirect users after login.
